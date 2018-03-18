9369e116117545e1ca9b38348ab511a93852a9b7
t_a_ydersbond <t_a_ydersbond@yahoo.no>
2018-03-18 20:32:47 +0100

    Updated for blog

--- prime_testing.R
+++ prime_testing.R
@@ -9,6 +9,21 @@ source("sieve.R")
 
 
 #
+graphics.off()
+
+#
+#
+#
+#
+
+testSieve <- function(MAXN) {
+
+    print(t1 <- system.time(p1 <- sieveLoop(MAXN)))
+    print(t2 <- system.time(p1 <- sieveVector(MAXN)))
+    c(MAXN,t1[3],t2[3])
+}
+
+
 
 timeSieve <- function(MAXN=10000) {
 
@@ -17,13 +32,54 @@ timeSieve <- function(MAXN=10000) {
 }
 
 
+comparePrimeSieves <- function(firstN=10000,nSteps=10,stepSize=2){
+
+    currSize <- firstN ;
+    resM <- matrix(0,nrow=nSteps,ncol=3,byrow=T)
+    for (i in 1:nSteps) {
+        t1 <- testSieve(currSize) ;
+        resM[i,] <- c(currSize,t1[2],t1[3])    
+        currSize <- currSize * stepSize ;
+        if (currSize>500000) print(paste("CurrSize: ",currSize)) ;
+    }
+
+    resM
+}
+
+
+plotComparison <- function(lc0) {
+
+    plotter <- function()  {
+        plot(lc0[,1],lc0[,2],col=2,xlab="log(N)",ylab="log(Time,sec)",ylim=c(-7,6))
+        points(lc0[,1],lc0[,3],col=3)
+        abline(lmloop$coef[1],lmloop$coef[2],col=2,lty=3)
+        abline(lmvect$coef[1],lmvect$coef[2],col=3,lty=3)
+        legend(13,-4,lty=c(3,3),col=c(2,3),
+               legend=c(paste("Loop, T= ",round(exp(lmloop$coef[1])*1000000,4),"*","N^",round(lmloop$coef[2],3)),
+                        paste("Vector, T= ",round(exp(lmvect$coef[1])*1000000,4),"*","N^",round(lmvect$coef[2],3))))
+    }   
+
+    lmloop <- lm(lc0[,2]~lc0[,1])
+    lmvect <- lm(lc0[,3]~lc0[,1])
+     
+    X11() ;  plotter() ;
+    png(file="sieve_loop_vector.png") ;  plotter() ;  dev.off() ;
+        
+}
+
+
+compareAndPlotComparisons <- function(nSteps=15) {
+    c0 <-comparePrimeSieves(nSteps=15)
+    lc0 <- log(c0)  
+    plotComparison(lc0) 
+}
 
 
 
 timeAndCountPrimes <- function(firstN=100,nSteps=10,stepSize=2){
 
     currSize <- firstN ;
-    resM <- matrix(0,nrow=nSteps,ncol=3,byrow=3)
+    resM <- matrix(0,nrow=nSteps,ncol=3,byrow=T)
     for (i in 1:nSteps) {
         t1 <- system.time(p1 <- sieveVector(currSize)) ;
         resM[i,] <- c(currSize,t1[3],length(p1))    
@@ -34,25 +90,32 @@ timeAndCountPrimes <- function(firstN=100,nSteps=10,stepSize=2){
     resM
 }
 
-primeProperties <- function() {
+primeProperties <- function(tc1) {
 
-   tc1 <- timeAndCountPrimes(nSteps=20)
+    plotterPNT <- function() {
+        plot(log(tc1[,1]),tc1[,1]/tc1[,3],col=2,ylim=c(4,18),xlab="log(N)",ylab="N/pi(N)")
+        points(log(tc1[,1]),log(tc1[,1])-1,type="l",lty=3,col=2)
+        legend(12,8,lty=c(3),col=c(2),legend=c("Y=log(N)-1"))
+
+    }
 
     
-# 1 Prime number theorem illustrated x/pi(x) = log(x)-1
-   X11() 
-   plot(log(tc1[,1]),tc1[,1]/tc1[,3],col=2,ylim=c(4,18))
-   points(log(tc1[,1]),log(tc1[,1])-1,type="l")
-
-# 2 Timing in R vector implementation
-    lf <- lm(log(tc1[6:20,2]) ~ log(tc1[6:20,1]))
-   # line y = -17.1 + 1.15x    
-   # ie in this case approx time = 1/exp(17.1)*N^1.15
     
-    X11()
-    plot(log(tc1[,1]),log(tc1[,2]),col=2)  
-    abline(coef(lf)[1],coef(lf)[2]) 
- 
+ # 1 Prime number theorem illustrated x/pi(x) = log(x)-1
+    X11() ;  plotterPNT() ;
+    png(file="sieve_PNT.png") ;  plotterPNT() ;  dev.off() ;
+    
+    
+
+ # 2 Timing in R vector implementation
+ #   lf <- lm(log(tc1[6:20,2]) ~ log(tc1[6:20,1]))
+ # line y = -17.1 + 1.15x    
+ # ie in this case approx time = 1/exp(17.1)*N^1.15
+    
+  #  X11()
+  #  plot(log(tc1[,1]),log(tc1[,2]),col=2)  
+  #  abline(coef(lf)[1],coef(lf)[2]) 
+    
     
 }
 
@@ -79,3 +142,7 @@ primeProperties <- function() {
 #[18,] 13107200  6.628  855733
 #[19,] 26214400 16.144 1637055
 #[20,] 52428800 41.951 3137913
+
+
+# tc1 <- timeAndCountPrimes(nSteps=20)    
+# primeProperties(tc1)
--- sieve.R
+++ sieve.R
@@ -5,67 +5,60 @@
 #
 
 
-sieveLoop <- function(MAXN=10000){
-
+sieveLoop0 <- function(MAXN=10000){
   numbers <- rep(0,MAXN) ; nseq <- seq(1:MAXN) ;
   maxSmallFactor <- round(sqrt(MAXN)) ;
 
   currPrime <- 2 ; nPrimes <- 0 ; numbers[1] <- 1 ;
   while(currPrime <= maxSmallFactor) {
       nPrimes <- nPrimes + 1 ;
-      checkNum <- 2*currPrime ;
+      checkNum <- currPrime*currPrime ;
       while(checkNum<=MAXN){
           numbers[checkNum] <- 1 ;
           checkNum <- checkNum + currPrime ;
       }
       currPrime <- currPrime + 1 ;
       while (numbers[currPrime]==1)  currPrime <- currPrime + 1 ;
-      
+  }
+  nseq[numbers==0]
+}
+
+sieveLoop <- function(MAXN=1000){
+  numbers <- seq(1:MAXN) ; 
+  maxSmallFactor <- round(sqrt(MAXN)) ;
+  currPrime <- 2 ;   numbers[1] <- 0 ;
+  while(currPrime <= maxSmallFactor) {
+      checkNum <- currPrime*currPrime ;
+      while(checkNum<=MAXN){
+          numbers[checkNum] <- 0 ;
+          checkNum <- checkNum + currPrime ;
+      }
+      currPrime <- currPrime + 1 ;
+      while (numbers[currPrime]==0)  currPrime <- currPrime + 1 ;
   }
 
-  nseq[numbers==0]
-  
-
+  numbers[numbers!=0]
 }
 
 
 
-sieveVector <- function(MAXN=10000){
-
+sieveVector <- function(MAXN=1000){
     numbers <- 3:MAXN
     maxSmallFactor <- round(sqrt(MAXN)) ;
-
     currPrime <- 2 ; primes <- c(2) 
     while(currPrime <= maxSmallFactor) {
-
         numbers <- numbers[numbers %% currPrime!=0]
         currPrime <- numbers[1] ;  
         primes <- c(primes,currPrime)
-        
     }
-    
     primes <- c(primes,numbers)
-    
     primes
-    
 }
 
 
 
 sieve <-  function(MAXN){
     sieveVector(MAXN)
-}
-
-#
-#
-#
-#
-
-testSieve <- function(MAXN) {
-
-    print(system.time(p1 <- sieveLoop(MAXN)))
-    print(system.time(p1 <- sieveVector(MAXN)))
-
 }
 
 
